6446c5d2bfc47c18b18f7e8e1ab8d6b2948972fe
felix <salgadocanga@gmail.com>
2024-06-07 16:08:31 -0500

    se agrego hicieron cambios en la estructura de carpetas

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-const {app,BrowserWindow, ipcMain} = require("electron")
-const path = require("path")
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
-const {validarUsuario} = require("./crud")
-const db = new sqlite3.Database(path.join(path.join(__dirname,"/db","data.db")))
-
+const { validarUsuario } = require('./crud');
+const db = new sqlite3.Database(
+    path.join(path.join(__dirname, '/db', 'data.db'))
+);
 
 let windowLogin;
-function createWindow (){
-    let dato = "hola mundo"
+let indexHtml = './src/views/index.html';
+// funcion para crear la ventana
+function createWindow() {
     windowLogin = new BrowserWindow({
         width: 600,
         height: 400,
         webPreferences: {
             nodeIntegration: true,
-            preload: path.join(__dirname, 'preload.js')
-        }
-    })
-   
-    windowLogin.loadFile("index.html")
-    
+            preload: path.join(__dirname, 'preload.js'),
+        },
+    });
+    windowLogin.loadFile(indexHtml);
 }
 
 /* if (process.env.NODE_ENV !== 'production') {
@@ -39,32 +39,31 @@ function createWindow (){
     })
 } */
 
-app.whenReady().then(()=>{
-   /*  ipcMain.on("informacion",(event,i)=>{
-      console.log(i)  
+app.whenReady().then(() => {
+    /*  ipcMain.on("informacion",(event,i)=>{
+      console.log(i)
     }) */
-    createWindow()
-})
+    createWindow();
+});
 
-ipcMain.on("validacion",(e,datos) => {
-
-validarUsuario(db,datos,(mensajeValidaciones)=>{
-
-   if(mensajeValidaciones.length !== 2){
-    windowLogin.webContents.send("mensajes","Error en el usuario y/o contraseña")
-   }else{
-    let windowMain = new BrowserWindow({
-        width: 1260,
-        height: 840,
-        webPreferences: {
-            nodeIntegration: true,
-            preload: path.join(__dirname, 'preload.js')
+ipcMain.on('validacion', (e, datos) => {
+    validarUsuario(db, datos, (mensajeValidaciones) => {
+        if (mensajeValidaciones.length !== 2) {
+            windowLogin.webContents.send(
+                'mensajes',
+                'Error en el usuario y/o contraseña'
+            );
+        } else {
+            let windowMain = new BrowserWindow({
+                width: 1260,
+                height: 840,
+                webPreferences: {
+                    nodeIntegration: true,
+                    preload: path.join(__dirname, 'preload.js'),
+                },
+            });
+            windowLogin.close();
+            windowMain.show();
         }
-    })
-    windowLogin.close()
-    windowMain.show()
-   }
-   
-})
-
-})
+    });
+});
